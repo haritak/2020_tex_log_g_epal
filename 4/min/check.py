47533716542cf77e84b2f6a83314059de43f8e89
Charitakis Yannis <github@ych.gr>
2020-01-29 05:36:04 +0200

    Ασκηση με ελάχιστη τιμή

---
+++ 4/min/check.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from solution import solution
+import random
+import sys
+
+P =  [0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 5, 5, 6, 8, 8, 9, 12, 21, 21, 23, 32, 34, 42, 65, 123, 123, 213, 213, 312, 321, 321, 432, 765, 876, 3213, 6765, 678213]
+i = solution(P)
+
+print "Check 1: passed - No syntax errors"
+
+if i == solution(P):
+    print "Check 2: passed - Σωστή ελάχιστη τιμή"
+else:
+    print "Check 2: FAILED - Λάθος ελάχιστη τιμή"
+
+for j in range(1, 10):
+    L = random.sample(range(1,1000), 100)
+    i = solution(L)
+    if i == min(L):
+        print "Check",j+2,": passed - Σωστή ελάχιστη τιμή"
+    else:
+        print "Check",j+2,": FAILED - Λάθος ελάχιστη τιμή"
+        sys.exit()
+
+
