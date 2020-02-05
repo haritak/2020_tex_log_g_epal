91268bb37d436608ca8c928b3d5c425c2d05ec9c
Charitakis Yannis <github@ych.gr>
2020-02-05 09:10:00 +0200

    Simple for 2 plus a fix on the previous exercise

--- 4/simple_for2/check.py
+++ 4/simple_for2/check.py
@@ -6,9 +6,9 @@ from solution import solution
 l = solution()
 print "Check 1: passed - No syntax errors"
 
-j1 = [i for i in range(0,6)]
-j2 = [i for i in range(6,33,2)]
-j3 = [i for i in range(33,43,2)]
+j1 = [i for i in range(100,49,-2)]
+j2 = [i for i in range(49,10,-2)]
+j3 = [i for i in range(10,0,-1)]
 #print j1,j2,j3
 j = j1+j2+j3
 #print j
