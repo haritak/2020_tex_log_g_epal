dbed6c179bc2b1a4da071fa23c3b578ad25f8db0
Charitakis Yannis <github@ych.gr>
2020-02-13 11:38:21 +0200

    New problem on files - difficult

---
+++ 6/chessboard/check.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from solution import solution
+
+solution()
+print "Check 1: passed - No syntax errors"
+
+f = open("chessboard.txt", "w")
+f.write("_________________\n")
+for i in range(8):
+    f.write(". . . . . . . . .\n")
+f.write("-----------------\n")
+f.close()
+solution()
+
+f = open("chessboard.txt", "r")
+lineno = 0
+errors = 0
+for line in f:
+    corrent_line = "";
+    if lineno==0:
+        corrent_line = "_________________\n"
+    elif lineno==9:
+        corrent_line = "-----------------\n"
+    elif lineno%2==1:
+        corrent_line = ". .X. .X. .X. .X.\n"
+    elif lineno%2==0:
+        corrent_line = ".X. .X. .X. .X. .\n"
+    if line==corrent_line:
+        print "Line number ", lineno, "is ok."
+    else:
+        print "Line number ", lineno, "is DESTROYED!"
+        errors+=1
+
+    lineno+=1
+
+if errors == 0:
+    print "Check 2: passed - Η σκακιέρα είναι τέλεια!"
+else:
+    print "Check 2: FAILED - Η σκακιέρα έχει προβλήματα!"
+
+
+f.close()
+f = open("chessboard.txt", "w")
+f.write("_________________\n")
+for i in range(8):
+    f.write("| | | | | | | | |\n")
+f.write("-----------------\n")
+f.close()
+print "End of checks!"
+
